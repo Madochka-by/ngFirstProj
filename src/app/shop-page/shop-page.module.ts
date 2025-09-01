32e1d3bababc1b15481938a738e37c127b17cf5c
Davydenko Kirill <davydenko_05@inbox.ru>
2025-09-01 20:36:50 +0300

    add pagination

--- src/app/shop-page/shop-page.module.ts
+++ src/app/shop-page/shop-page.module.ts
@@ -5,10 +5,17 @@ import { BannerModule } from '../banner/banner.module';
 import { ShopPageRouting } from './shop-page-routing.module';
 import { OnfooterModule } from '../onfooter/onfooter.module';
 import { CardModule } from 'src/app/card/card.module';
+import { PaginatorModule } from 'primeng/paginator';
 
 @NgModule({
   declarations: [ShopComponent],
   exports: [ShopComponent, ShopPageRouting],
-  imports: [CommonModule, BannerModule, OnfooterModule, CardModule],
+  imports: [
+    CommonModule,
+    BannerModule,
+    OnfooterModule,
+    CardModule,
+    PaginatorModule,
+  ],
 })
 export class ShopPageModule {}
--- src/app/shop-page/shop/shop.component.ts
+++ src/app/shop-page/shop/shop.component.ts
@@ -9,6 +9,7 @@ import { FlatMapService } from 'src/app/functionForAllProject/FlatMap/flat-map.s
 })
 export class ShopComponent implements OnInit {
   public allData: any[] = [];
+  public currentData: any[] = [];
 
   constructor(
     private _getData: GetDBDataService,
@@ -18,6 +19,13 @@ export class ShopComponent implements OnInit {
   ngOnInit(): void {
     this._getData.getData().subscribe((res: any) => {
       this.allData = this._proccessingFunc.bringingDataIntoLine(res);
+
+      this.currentData = this.allData.slice(0, 8);
     });
+  }
+  onPageChange(event: any) {
+    const start = event.first;
+    const end = start + event.rows;
+    this.currentData = this.allData.slice(start, end);
   }
 }
